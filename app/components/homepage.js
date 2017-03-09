3523091e637877fffeb174c20e50ceeb38b05608
Bryce Bodley-Gomes <bbodleygomes@umass.edu>
2017-03-09 16:38:41 -0500

    merging navbar changes into master

---
+++ app/components/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NAVBAR from './navbar.js';
+
+
+export default class HOMEPAGE extends React.Component {
+  render() {
+    return (
+      <div>
+        <NAVBAR  />
+        <h1> homepage content </h1> //place holder will be replaced with other components
+      </div>
+
+    )
+  }
+}
---
+++ app/components/ui-02.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export default class UI02 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is UI #2</h1>
+
+      </div>
+    )
+  }
+}
