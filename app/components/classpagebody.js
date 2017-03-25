5ca007b62774022620ddf181bee0ffc27da386c5
mfeuerstein <michael.a.feuerstein@gmail.com>
2017-03-25 14:19:11 -0400

    Fixed Item styling

--- app/components/classpagebody.js
+++ app/components/classpagebody.js
@@ -22,10 +22,10 @@ export default class CLASSPAGEBODY extends React.Component {
           <div className="row">
             <div className="col-md-2 fb-left-sidebar"></div>
             <div className="col-md-8">
-              <h1 className="page-header">Books</h1>
+              <h2 className="page-header">Books</h2>
               <div className="row">
                 <ITEM description="How to own an interview" referance="img/book1.jpg" price = "18.00"/>
-                <ITEM description=" Not as good as the internet" referance="img/book2.jpg" price = "21.00"/>
+                <ITEM description="Not as good as the internet" referance="img/book2.jpg" price = "21.00"/>
                 <ITEM description="Intro to $$$" referance="img/book3.jpg" price = "25.00"/>
               </div>
             </div>
@@ -35,7 +35,7 @@ export default class CLASSPAGEBODY extends React.Component {
             <div className="col-md-2 fb-left-sidebar">
             </div>
             <div className="col-md-8">
-              <h1 className="page-header">Tech</h1>
+              <h2 className="page-header">Tech</h2>
               <div className="row">
                 <ITEM description="Lecture prison key" referance="img/iclicker-profile.jpg" price = "98.00"/>
               </div>
