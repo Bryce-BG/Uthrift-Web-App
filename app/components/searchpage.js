f953afa6e6baf7f27ee2fbb740f8b6db3b54dc98
Yi Fung <yirenfung@gmail.com>
2017-03-25 16:07:39 -0400

    My more reactified search page

--- app/components/searchpage.js
+++ app/components/searchpage.js
@@ -7,23 +7,20 @@ export default class SEARCHPAGE extends React.Component{
 	render(){
 		return(
 			<div>
-		    	<NAVBAR />
-		    	<div className="container content-contain">
-	  				<div className="row">
-	        			<div className="col-md-2 fb-left-sidebar">
-	        			</div>
-	        			<div className="col-md-7">
-	        				<h1 className="page-header">Search Result    
-				                <small> "Books on how to hack life"</small>
-				            </h1>
-				            <SEARCHRESULTS />
-				            <SEARCHPAGINATION />
-	        			</div>
-	        			<div className="col-md-2 fb-right-sidebar">
-	        			</div>
-	        		</div>
-        		</div>
-		 	</div>
-	 	)
- 	}
+				<NAVBAR />
+				<div className="container content-contain">
+					<div className="row">
+						<div className="col-md-2 fb-left-sidebar">
+						</div>
+						<div className="col-md-7">
+							<SEARCHRESULTS query=" Books on how to hack life"/>
+							<SEARCHPAGINATION />
+						</div>
+						<div className="col-md-2 fb-right-sidebar">
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
