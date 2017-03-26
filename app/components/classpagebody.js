d48782e1b81d38a3150fe2229dc3d05d288c0c09
mfeuerstein <michael.a.feuerstein@gmail.com>
2017-03-26 17:20:02 -0400

    Minor changes to classpage.css and renamed item to classitem

--- app/components/classpagebody.js
+++ app/components/classpagebody.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ITEM from './item.js';
+import CLASSITEM from './classitem.js';
 import CLASSINFO from './classinfo.js';
 
 export default class CLASSPAGEBODY extends React.Component {
@@ -20,24 +20,24 @@ export default class CLASSPAGEBODY extends React.Component {
       <div className="tab-content Items">
         <div id="Material" className="tab-pane fade in active">
           <div className="row">
-            <div className="col-md-2 fb-left-sidebar"></div>
+            <div className="col-md-2"></div>
             <div className="col-md-8">
               <h2 className="page-header">Books</h2>
               <div className="row">
-                <ITEM description="How to own an interview" reference="img/book1.jpg" price = "18.00"/>
-                <ITEM description="Not as good as the internet" reference="img/Books/8.png" price = "21.00"/>
-                <ITEM description="Intro to $$$" reference="img/book3.jpg" price = "25.00"/>
+                <CLASSITEM description="How to own an interview" reference="img/book1.jpg" price = "18.00"/>
+                <CLASSITEM description="Not as good as the internet" reference="img/book2.jpg" price = "21.00"/>
+                <CLASSITEM description="Intro to $$$" reference="img/book3.jpg" price = "25.00"/>
               </div>
             </div>
           </div>
           <hr/>
           <div className="row">
-            <div className="col-md-2 fb-left-sidebar">
+            <div className="col-md-2">
             </div>
             <div className="col-md-8">
               <h2 className="page-header">Tech</h2>
               <div className="row">
-                <ITEM description="Lecture prison key" reference="img/iclicker-profile.jpg" price = "98.00"/>
+                <CLASSITEM description="Lecture prison key" reference="img/iclicker-profile.jpg" price = "98.00"/>
               </div>
             </div>
           </div>
