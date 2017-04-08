84f5ac702e0dce475d06b3830b3cc6932309345a
Bryce Bodley-Gomes <bbodleygomes@umass.edu>
2017-04-08 00:46:32 -0400

    fixing typos on navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -18,6 +18,7 @@ export default class NAVBAR extends React.Component {
                       <li className="upper-links"><a className="links" href="#">Contact Us</a></li>
                       <li className="upper-links"><a className="links" href="#">Help</a></li>
                       <li className="upper-links"><a className="links" href="#">Sell</a></li>
+                      <li className="upper-links"><a className="links" href="#">Profile</a></li>
                       {/*<li className="upper-links"><Link to="account">Account Info</Link> </li>*/}
 
                   </ul>
@@ -37,7 +38,7 @@ export default class NAVBAR extends React.Component {
 
                                    <div className="col-md-3" id = "catogory-Select" >
                                      <select name="catogories" className="form-control dropdown">
-                                      <option value = "Cagogory">Select Catogory</option>
+                                      <option value = "Category">Select Category</option>
                                       <option value="Textbooks">Textbooks</option>
                                       <option value="Clothing">Clothing</option>
                                       <option value="Tech">Tech</option>
@@ -48,9 +49,9 @@ export default class NAVBAR extends React.Component {
                                    </div>
 
 
-                                     <input className="uthrift-navbar-input col-md-8" type="text" placeholder="Search UThrift..." name="search"/>
+                                     <input className="uthrift-navbar-input col-md-8" value = "" type="text" placeholder="Search UThrift..." name="search"/>
 
-                                     <button className="uthrift-navbar-button col-md-1">
+                                     <button className="uthrift-navbar-button col-md-1" >
                                          <svg width="15px" height="15px">
                                              <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
                                          </svg>
