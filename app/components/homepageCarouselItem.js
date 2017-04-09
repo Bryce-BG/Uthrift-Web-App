06e6c0d6e7d87d437cb6e761b7855ff6f1a7be31
Bryce Bodley-Gomes <bbodleygomes@umass.edu>
2017-04-09 16:32:43 -0400

    fixed title and slider items for hte carousel

--- app/components/homepageCarouselItem.js
+++ app/components/homepageCarouselItem.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 
+
 export default class HOMEPAGECAROUSELITEM extends React.Component
 {
+
+
+
   render() {
+    console.log(this.props.name);
     return (
       <div className = "col-md-3 middle-carousel-item">
           <h3>{this.props.name}</h3>
           <img className ="img-responsive" src={this.props.referance} alt= {this.props.name}/>
           <hr />
-          <p>price: {this.props.price} </p>
-          <a href="#">Item Sellers</a>
+          <p>price: ${this.props.price} </p>
+          
+
       </div>
     )
   }
