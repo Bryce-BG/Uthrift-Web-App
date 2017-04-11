20d81f9b3753c530f91bc7285b6d504b44ebc5c1
mfeuerstein <michael.a.feuerstein@gmail.com>
2017-04-11 14:30:56 -0400

    Altered classitem to use "Link to" instead of "href"

--- app/components/classitem.js
+++ app/components/classitem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import {getItemData} from '../server';
-//import {Link} from 'react-router';
+import {Link} from 'react-router';
 
 export default class CLASSITEM extends React.Component{
 
@@ -8,9 +8,9 @@ export default class CLASSITEM extends React.Component{
     //console.log(this.props);
     return (
       <div className = "classitem col-md-4">
-          <a href={"#/ItemPage/" + this.props.item._id}>
+          <Link to={"/ItemPage/" + this.props.item._id}>
             <img src={this.props.item.photoRef} alt=""/>
-          </a>
+          </Link>
           <h5 className="description">{this.props.item.Description}</h5>
           <h4 className="price">${this.props.item.Price}</h4>
           {/*<h3>{this.props.name}</h3>*/}
