4b963ec392cf222eba20445d4f01d5bc4bbbe5f1
Bryce Bodley-Gomes <bbodleygomes@umass.edu>
2017-04-21 16:04:16 -0400

    got rid of dead code

--- client/app/app.js
+++ client/app/app.js
@@ -1,67 +1,3 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-// Each major browser view user interface must be imported.
-import HOMEPAGE from './components/homepage.js';
-import PROFILEPAGE from './components/profilepage.js';
-import SEARCHPAGE from './components/searchpage.js';
-import ITEM from './components/item.js';
-import CLASSPAGE from './components/classpage.js';
-import SUBMISSIONFORM from './components/submissionform/submissionform.js';
-
-// For each view conditionally determine which view to display
-// depending on if the ID is present in the HTML.
-
-
-
-const loggedInUserid= "1";
-
-
-if (document.getElementById('homepage') !== null) {
-  ReactDOM.render(
-    <HOMEPAGE UserID = {loggedInUserid} />,
-    document.getElementById('homepage')
-  );
-}
-
-
-else if (document.getElementById('profilepage') !== null) {
-  ReactDOM.render(
-    <PROFILEPAGE UserID = {loggedInUserid} />,
-    document.getElementById('profilepage')
-  );
-}
-
-
-else if (document.getElementById('searchpage') !== null) {
-  ReactDOM.render(
-    <SEARCHPAGE UserID = {loggedInUserid}/>,
-    document.getElementById('searchpage')
-  );
-}
-
-else if (document.getElementById('item') !== null) {
-  ReactDOM.render(
-    <ITEM UserID = {loggedInUserid}/>,
-    document.getElementById('item')
-  );
-}
-
-else if (document.getElementById('classpage') !== null) {
-  ReactDOM.render(
-    <CLASSPAGE UserID = {loggedInUserid}/>,
-    document.getElementById('classpage')
-  );
-}
-
-else if (document.getElementById('submissionform') !== null) {
-  ReactDOM.render(
-    <SUBMISSIONFORM UserID = {loggedInUserid}/>,
-    document.getElementById('submissionform')
-  );
-}*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HOMEPAGE from './components/homepage.js';
@@ -134,15 +70,6 @@ class App extends React.Component {
 
       </div>
 
-      // <div>
-      // <div>
-      //   <NAVBAR  user = {loggedinuser} />
-      // </div>
-      // <div>
-      //   <ErrorBanner />
-      // </div>
-      //
-      // </div>
     )
   }
 }
