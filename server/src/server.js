7d625295fb0dadfe6ee41496603a66d8e9eddfdb
Ryan Semple <Ryan.Semple@Comcast.net>
2017-04-24 14:04:21 -0400

    changed submit item to create new item instead of changing item1s info

--- server/src/server.js
+++ server/src/server.js
@@ -203,24 +203,27 @@ app.put('/profile', validate({ body:  UserDataSchema }), function(req, res) {
 //submission form junk
 function submitItem(title, price, condition, conDesc, classRelated,
     subject, courseNumber, category, categoryDescription, photoRef, sold, sellerId){
-  var itemData = readDocument('items', 1);
+  //var itemData = readDocument('items', 1);
   var time = new Date().getTime();
 
   var itemID = (Object.keys(getArray('items')).length) + 1;
-  itemData._id = itemID; //not sure if this should be itemData.itemId because the json has itemId instead of _id
-  itemData.postDate = time;
-  itemData.Title = title;
-  itemData.Price = price;
-  itemData.Condition = condition;
-  itemData.Description = conDesc;
-  itemData.classRelated = classRelated;
-  itemData.subject = subject;
-  itemData.courseNumber = courseNumber;
-  itemData.Category = category;
-  itemData.categoryDescription = categoryDescription;
-  itemData.photoRef = "img/iclicker.jpg";
-  itemData.Sold = sold;
-  itemData.SellerId = sellerId;
+var itemData = {
+  "_id": itemID, //not sure if this should be itemData.itemId because the json has itemId instead of _id
+  "postDate": time,
+  "Title": title,
+  "Price": price,
+  "Condition": condition,
+  "Description": conDesc,
+  "classRelated": classRelated,
+  "subject": subject,
+  "courseNumber": courseNumber,
+  "Category": category,
+  "categoryDescription": categoryDescription,
+  "photoRef": "img/iclicker.jpg",
+  "Sold": sold,
+  "SellerId": sellerId
+};
+  addDocument('items', itemData);
   writeDocument('items', itemData);
 
   //Update selling list by copying seller profile and adding item # to array
@@ -235,9 +238,9 @@ app.post('/submissionForm/', validate({ body:  ItemsSchema }), function(req, res
   var body = req.body;
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   if (fromUser === body.sellerId) {
-    submitItem(body.Title, body.Price, body.Condition, body.Description, body.classRelated, body.subject,
+    var newItem = submitItem(body.Title, body.Price, body.Condition, body.Description, body.classRelated, body.subject,
       body.courseNumber, body.Category, body.categoryDescription, body.photoRef, body.Sold, body.SellerId);
-    res.send(getUserData(body.SellerId));
+    res.send(newItem); //not sure what this should be
   } else {
     res.status(401).end();
   }
